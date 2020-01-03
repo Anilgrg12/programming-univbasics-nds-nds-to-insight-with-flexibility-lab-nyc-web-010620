ddd21b2785f1602c5705e6f2ea63f7843abde1a6
amusing-transpiler-2573 <anil_grg@hotmail.com>
2020-01-03 07:34:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,7 +1,9 @@
 # Provided, don't edit
 require 'directors_database'
+require pp
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
+end
 
 def flatten_a_o_a(aoa)
   result = []
